0ed4c370e8b2e4c258e2fd807f73626273edd960
Nate Jensen <najens@gmail.com>
2018-05-04 14:33:31 -0600

    docs: Add routes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import LoadingBar from 'react-redux-loading'
+import Dashboard from './Dashboard'
+import QuestionPage from './QuestionPage'
+import NewQuestion from './NewQuestion'
+import Leaderboard from './Leaderboard'
 
 class App extends Component {
+  componentDidMount () {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
-      <div>
-          Hello World
-      </div>
-    );
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+              {this.props.loading === true
+                ? null
+                : <div>
+                    <Route path='/' exact component={Dashboard} />
+                    <Route path='/question/:id' component={QuestionPage} />
+                    <Route path='/add' component={NewQuestion} />
+                    <Route path='/leaderboard' component={Leaderboard} />
+                  </div>
+              }
+          </div>
+        </Fragment>
+      </Router>
+    )
   }
 }
 
-export default App;
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
