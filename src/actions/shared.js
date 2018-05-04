4b2149ad04e76281223023265fd461616a716fba
Nate Jensen <najens@gmail.com>
2018-05-04 12:46:01 -0600

    docs: Fix import typo

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
-import { receiveQuestions } from './tweets'
+import { receiveQuestions } from './questions'
 import { setAuthedUser } from './authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
