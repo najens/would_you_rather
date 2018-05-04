2888cbe88306c93150c4ed3f9736279aeafbc2b9
Nate Jensen <najens@gmail.com>
2018-05-04 12:33:30 -0600

    docs: Fix import typo

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,4 @@
-import { saveQuestion, saveQuestionsAnswer } from '../utils/api'
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
